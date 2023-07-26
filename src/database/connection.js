87c55c92fc3a96d6e2630fc15896855ca9dde2e4
roger rodriguez <rogerdavid.rd@gmail.com>
2023-07-26 17:15:53 -0400

    se cambia modificacion de connetion

--- src/database/connection.js
+++ src/database/connection.js
@@ -1,63 +1,63 @@
-// import * as dotenv from "dotenv";
-// dotenv.config();
+import * as dotenv from "dotenv";
+dotenv.config();
 
-// import pkg from "pg";
-// const {Pool} = pkg;
-
-
-// const connectionString = process.env.PG_URL;
-
-// export const pool = connectionString
-//   ? new Pool({
-//       connectionString,
-//       ssl: {
-//         rejectUnauthorized: false,
-//       },
-//       allowExitOnIdle: true,
-//     })
-//   : new Pool({
-//       allowExitOnIdle: true,
-//     });
-
-// try {
-//   await pool.query("SELECT NOW()");
-//   console.log("Conexión a la base de datos exitosa.");
-// } catch (error) {
-//   console.log("Error al conectar a la base de datos:", error);
-// }
-
-// // export default pool;
 import pkg from "pg";
-const { Pool } = pkg;
+const {Pool} = pkg;
+
 
 const connectionString = process.env.PG_URL;
 
-const connectWithRetry = () => {
-  const pool = new Pool({
-    connectionString,
-    ssl: {
-      rejectUnauthorized: false,
-    },
-    allowExitOnIdle: true,
-  });
+export const pool = connectionString
+  ? new Pool({
+      connectionString,
+      ssl: {
+        rejectUnauthorized: false,
+      },
+      allowExitOnIdle: true,
+    })
+  : new Pool({
+      allowExitOnIdle: true,
+    });
 
-  pool.on("error", (err) => {
-    console.error("Error en la conexión a la base de datos:", err);
-    setTimeout(connectWithRetry, 5000); // Reintentar la conexión después de 5 segundos
-  });
+try {
+  await pool.query("SELECT NOW()");
+  console.log("Conexión a la base de datos exitosa.");
+} catch (error) {
+  console.log("Error al conectar a la base de datos:", error);
+}
 
-  pool.connect((err) => {
-    if (err) {
-      console.error("Error en la conexión a la base de datos:", err);
-      setTimeout(connectWithRetry, 5000); // Reintentar la conexión después de 5 segundos
-    } else {
-      console.log("Conexión a la base de datos exitosa.");
-    }
-  });
+// // export default pool;
+// import pkg from "pg";
+// const { Pool } = pkg;
 
-  return pool;
-};
+// const connectionString = process.env.PG_URL;
 
-const pool = connectionString ? connectWithRetry() : new Pool();
+// const connectWithRetry = () => {
+//   const pool = new Pool({
+//     connectionString,
+//     ssl: {
+//       rejectUnauthorized: false,
+//     },
+//     allowExitOnIdle: true,
+//   });
 
-export default pool;
+//   pool.on("error", (err) => {
+//     console.error("Error en la conexión a la base de datos:", err);
+//     setTimeout(connectWithRetry, 5000); // Reintentar la conexión después de 5 segundos
+//   });
+
+//   pool.connect((err) => {
+//     if (err) {
+//       console.error("Error en la conexión a la base de datos:", err);
+//       setTimeout(connectWithRetry, 5000); // Reintentar la conexión después de 5 segundos
+//     } else {
+//       console.log("Conexión a la base de datos exitosa.");
+//     }
+//   });
+
+//   return pool;
+// };
+
+// const pool = connectionString ? connectWithRetry() : new Pool();
+
+// export default pool;
