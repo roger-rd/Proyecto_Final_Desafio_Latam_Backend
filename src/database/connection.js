415e558b32a77d243db1284b143bd9fa7a53dc7b
Roger Rodriguez Pire <rogerrodriguez@MacBook-Pro-de-Roger.local>
2023-07-27 00:05:31 -0400

    probando error

--- src/database/connection.js
+++ src/database/connection.js
@@ -26,38 +26,3 @@ try {
   console.log("Error al conectar a la base de datos:", error);
 }
 
-// // export default pool;
-// import pkg from "pg";
-// const { Pool } = pkg;
-
-// const connectionString = process.env.PG_URL;
-
-// const connectWithRetry = () => {
-//   const pool = new Pool({
-//     connectionString,
-//     ssl: {
-//       rejectUnauthorized: false,
-//     },
-//     allowExitOnIdle: true,
-//   });
-
-//   pool.on("error", (err) => {
-//     console.error("Error en la conexión a la base de datos:", err);
-//     setTimeout(connectWithRetry, 5000); // Reintentar la conexión después de 5 segundos
-//   });
-
-//   pool.connect((err) => {
-//     if (err) {
-//       console.error("Error en la conexión a la base de datos:", err);
-//       setTimeout(connectWithRetry, 5000); // Reintentar la conexión después de 5 segundos
-//     } else {
-//       console.log("Conexión a la base de datos exitosa.");
-//     }
-//   });
-
-//   return pool;
-// };
-
-// const pool = connectionString ? connectWithRetry() : new Pool();
-
-// export default pool;
